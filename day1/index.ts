fbe99a3bfd83cff07c8fed2ee70bfbd9facda12e
Branko Daskijević <branko.dask@gmail.com>
2024-12-01 20:04:38 +0100

    day 1 part 1

--- day1/index.ts
+++ day1/index.ts
@@ -1,9 +1,44 @@
 import type { BunFile } from 'bun'
 
-const input = Bun.file("./input.txt")
+const input = Bun.file('./input.txt')
+
+const compareNumbers = (a: number, b: number) => a - b
 
 async function readFile(file: BunFile) {
-  console.log(await file.text())
+  const inputStrings = (await file.text()).split('\n')
+
+  const leftString: Array<number> = []
+  const rightStrings: Array<number> = []
+  const differences: Array<number> = []
+
+  for await (const line of inputStrings) {
+    const splitLine = line.split('   ')
+
+    leftString.push(Number(splitLine[0]))
+    rightStrings.push(Number(splitLine[1]))
+  }
+
+  leftString.pop()
+  rightStrings.pop()
+
+  const leftStringsSorted = leftString.sort(compareNumbers)
+  const rightStringsSorted = rightStrings.sort(compareNumbers)
+
+  for (let i = 0; i < leftStringsSorted.length; i++) {
+    if (leftStringsSorted[i] > rightStringsSorted[i]) {
+      const diff = leftStringsSorted[i] - rightStringsSorted[i]
+      differences.push(diff)
+    } else if (leftStringsSorted[i] < rightStringsSorted[i]) {
+      const diff = rightStringsSorted[i] - leftStringsSorted[i]
+      differences.push(diff)
+    } else {
+      differences.push(0)
+    }
+  }
+
+  const sumOfDifferences = differences.reduce((a, b) => a + b, 0)
+
+  return sumOfDifferences
 }
 
-readFile(input)
+console.log(await readFile(input))
