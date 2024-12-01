da4f5fd31c6762aad9962f2739abfc28ca678689
Branko Daskijević <branko.dask@gmail.com>
2024-12-01 20:43:54 +0100

    day 1 part 2

--- day1/index.ts
+++ day1/index.ts
@@ -9,7 +9,8 @@ async function readFile(file: BunFile) {
 
   const leftStrings: Array<number> = []
   const rightStrings: Array<number> = []
-  const differences: Array<number> = []
+  const duplicates: Array<number> = []
+  const similarityScore: Array<number> = []
 
   for await (const line of inputStrings) {
     const splitLine = line.split('   ')
@@ -18,27 +19,40 @@ async function readFile(file: BunFile) {
     rightStrings.push(Number(splitLine[1]))
   }
 
-  leftStrings.pop()
-  rightStrings.pop()
-
-  const leftStringsSorted = leftStrings.sort(compareNumbers)
-  const rightStringsSorted = rightStrings.sort(compareNumbers)
-
-  for (let i = 0; i < leftStringsSorted.length; i++) {
-    if (leftStringsSorted[i] > rightStringsSorted[i]) {
-      const diff = leftStringsSorted[i] - rightStringsSorted[i]
-      differences.push(diff)
-    } else if (leftStringsSorted[i] < rightStringsSorted[i]) {
-      const diff = rightStringsSorted[i] - leftStringsSorted[i]
-      differences.push(diff)
-    } else {
-      differences.push(0)
+  for (let i = 0; i < leftStrings.length; i++) {
+    for (let j = 0; j < rightStrings.length; j++) {
+      if (leftStrings[i] === rightStrings[j]) {
+        duplicates.push(1)
+      }
     }
+
+    if (duplicates.length !== 0) {
+      similarityScore.push(leftStrings[i] * duplicates.length)
+    }
+
+    duplicates.length = 0
   }
 
-  const sumOfDifferences = differences.reduce((a, b) => a + b, 0)
+  // PART 1
+  // leftStrings.pop()
+  // rightStrings.pop()
 
-  return sumOfDifferences
+  // const leftStringsSorted = leftStrings.sort(compareNumbers)
+  // const rightStringsSorted = rightStrings.sort(compareNumbers)
+
+  // for (let i = 0; i < leftStringsSorted.length; i++) {
+  //   if (leftStringsSorted[i] > rightStringsSorted[i]) {
+  //     const diff = leftStringsSorted[i] - rightStringsSorted[i]
+  //     differences.push(diff)
+  //   } else if (leftStringsSorted[i] < rightStringsSorted[i]) {
+  //     const diff = rightStringsSorted[i] - leftStringsSorted[i]
+  //     differences.push(diff)
+  //   } else {
+  //     differences.push(0)
+  //   }
+  // }
+
+  return similarityScore.reduce((a, b) => a + b, 0)
 }
 
 console.log(await readFile(input))
